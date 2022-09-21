8565443348e82d2799d4c7bbd28745359865941e
Francisco Reynoso <fjreynoso@pima.edu>
2022-09-21 14:23:31 -0700

    Added getStaticPaths

--- pages/users-ssg/[id].js
+++ pages/users-ssg/[id].js
@@ -1,5 +1,17 @@
 
 
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    paths: data.map(event => ({
+      params: { id: event.id.toString() }
+    })),
+    fallback: false
+  }
+}
+
 export const getStaticProps = async ({ params }) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
   const data = await response.json();
